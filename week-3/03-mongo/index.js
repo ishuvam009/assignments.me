5fa70f1caf1fc9458431859c80dd44bc5be3b83e
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-07-03 00:45:48 +0530

    Commit

--- week-3/03-mongo/index.js
+++ week-3/03-mongo/index.js
@@ -9,8 +9,8 @@ app.use(bodyParser.json());
 app.use("/admin", adminRouter)
 app.use("/user", userRouter)
 
-const PORT = 3000;
+// const PORT = 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(7560, () => {
+    console.log(`Server isssss running on port 7560`);
 });
--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -1,10 +1,26 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { User } = require("../db");
 
 // User Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement user signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const checkUserExist = await User.findOne({username});
+    if(!checkUserExist){
+        await User.create({
+            username,
+            password
+        })
+    res.json({
+        message: 'User created Sucessfully.',
+    })    
+    }else{
+        res.status(409).send('User already exist with this username.');
+    }
 });
 
 router.get('/courses', (req, res) => {
@@ -19,4 +35,4 @@ router.get('/purchasedCourses', userMiddleware, (req, res) => {
     // Implement fetching purchased courses logic
 });
 
-module.exports = router
+module.exports = router;
